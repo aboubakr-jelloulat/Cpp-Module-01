476084f6f5ed6517357863add95cece929ee6139
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-09-10 18:33:38 +0100

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+	/* Refrences :
+
+			https://codefinity.com/blog/References-vs-Pointers-in-C-plus-plus
+			https://www3.ntu.edu.sg/home/ehchua/programming/cpp/cp4_PointerReference.html
+
+
+
+	*/
+
+int main(void)
+{
+
+    std::string	string = "HI THIS IS BRAIN";
+
+	std::string	*stringPTR = &string;
+
+	std::string	&stringREF = string;
+
+    
+    std::cout << "	MEMORY ADDRESSES \n" << std::endl;
+
+    std::cout << "Address of string	: " << &string << std::endl;
+    std::cout << "Address in stringPTR	: " << stringPTR << std::endl;
+    std::cout << "Address of stringREF	: " << &stringREF << std::endl;
+    
+   
+    
+    std::cout << "\n	VALUES 	\n" << std::endl;
+    std::cout << "Value of string			: " << string << std::endl;
+    std::cout << "Value pointed by stringPTR	: " << *stringPTR << std::endl;
+    std::cout << "Value of stringREF		: " << stringREF << std::endl;
+    
+    return (0);
+}
